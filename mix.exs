8ee31848e7d2ff4cea5bcf2694b57c6017ee22b7
PsychicPlatypus <dzenismadzovic@gmail.com>
2023-12-03 18:32:04 +0100

    Prepare for v0.1.0

--- mix.exs
+++ mix.exs
@@ -7,12 +7,28 @@ defmodule ExtractQueryParams.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
       deps: deps()
     ]
   end
 
+  defp description() do
+    "Small, light-weight elixir package that turn keyword lists to SQL statements with variables attached."
+  end
+
   def application do
     []
+  end
+
+  defp package() do
+    [
+      name: "extract_query_params",
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/PsychicPlatypus/extract_query_params"}
+    ]
   end
 
   defp deps do
